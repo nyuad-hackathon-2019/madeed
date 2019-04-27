c2c05b98ea6d09472d52b9e57023451c65db0dce
Aaminah Khan <aaminahrizwan@gmail.com>
2019-04-27 17:58:00 +0400

    adding tabs and viewpager

--- app/src/main/java/com/android/madeed/MainActivity.java
+++ app/src/main/java/com/android/madeed/MainActivity.java
@@ -1,8 +1,13 @@
 package com.android.madeed;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,15 +17,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            Intent intent = new Intent(this, ResultsActivity.class);
+            startActivity(intent);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //search results activity
+
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -99,3 +117,4 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 }
+
