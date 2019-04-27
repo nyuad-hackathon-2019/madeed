25741badeac0d7e4f9b135edb74b2954844329e7
Yohan Rizk <yrizk@google.com>
2019-04-28 03:41:20 +0400

    back in business. dictionaryfragment working with the base fragment extension in place

--- app/src/main/java/com/android/madeed/MadeedApp.java
+++ app/src/main/java/com/android/madeed/MadeedApp.java
@@ -5,9 +5,16 @@ import android.content.Context;
 
 public class MadeedApp extends Application {
 
+    private static Context sInstance = null;
+
     @Override
     public void onCreate() {
+        sInstance = this;
         super.onCreate();
+    }
+
+    public static Context getContext() {
+        return sInstance;
     }
 
     public static MadeedApi getApi(Context context) {
--- app/src/main/java/com/android/madeed/ShowResults.java
+++ app/src/main/java/com/android/madeed/ShowResults.java
@@ -22,8 +22,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShowResults extends AppCompatActivity implements MadeedListener {
-
+public class ShowResults extends AppCompatActivity {
 
     private ViewPager viewPager;
     private PageAdapter myPagerAdapter;
@@ -32,17 +31,12 @@ public class ShowResults extends AppCompatActivity implements MadeedListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_results);
-
-        setUpViewPager();
-
-
-
-
+        setUpViewPager(getIntent().getStringExtra(MainActivity.POSITION_MESSAGE));
     }
 
-    private void setUpViewPager() {
+    private void setUpViewPager(final String query ) {
         viewPager = (ViewPager) findViewById(R.id.viewPager);
-         myPagerAdapter = new PageAdapter(getSupportFragmentManager());
+        myPagerAdapter = new PageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(myPagerAdapter);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
@@ -55,6 +49,7 @@ public class ShowResults extends AppCompatActivity implements MadeedListener {
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition(), true);
 
             }
 
@@ -62,29 +57,6 @@ public class ShowResults extends AppCompatActivity implements MadeedListener {
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition(), true);
             }
-
         });
-    }
-
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.POSITION_MESSAGE);
-        final MadeedApi madeedApi = MadeedApp.getApi(getApplicationContext());
-        madeedApi.define(message, ShowResults.this);
-    }
-
-    @Override
-    public void onTermDefinitionComplete(String originalTerm, List<Word> words) {
-
-        DictionaryFragment df = (DictionaryFragment) myPagerAdapter.getItem(viewPager.getCurrentItem());
-        df.setData(words);
-    }
-
-    @Override
-    public void onSuggestionLookupComplete(String originalTerm, List<String> words) {
-        Log.e("Madeed", words.toString());
     }
 }
